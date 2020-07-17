bae82e2b1a53db9b915940c8d27009f10d6e89ac
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-17 21:14:54 +0300

    master

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const numbers = [5, 6, 8, 11, 9, 4];
-
-function reverseArray(arr) {
-  if (!Array.isArray(arr)) {
-    return null;
-  }
-  return arr.reverse();
-}
-
-console.log(reverseArray(numbers));
-
-//клон массива===========================
-
 // const numbers = [5, 6, 8, 11, 9, 4];
 
-// function cloneArr(arr) {
+// function reverseArray(arr) {
 //   if (!Array.isArray(arr)) {
 //     return null;
 //   }
-
-//   return (newArray = arr.slice());
+//   return arr.reverse();
 // }
-// console.log(cloneArr(numbers));
+
+// console.log(reverseArray(numbers));
+
+//клон массива===========================
+
+const numbers = [5, 6, 8, 11, 9, 4];
+
+function cloneArr(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+
+  return (newArray = arr.slice());
+}
+console.log(cloneArr(numbers));
