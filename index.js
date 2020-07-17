da35af8de1b290e9b732d0c7fab415c585073337
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-17 21:48:05 +0300

    master

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function cloneArr(arr) {
+function reverseArray(arr) {
   if (!Array.isArray(arr)) {
     return null;
   }
-  let newArray = arr.slice();
-  return newArray;
+  return arr.reverse();
 }
