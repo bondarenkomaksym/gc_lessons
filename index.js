7805527c158e4766d8a456f63a07213931aa868a
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-18 11:38:21 +0300

    master

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-function sortAsc(array) {
-  if (!Array.isArray(array)) {
-    return null;
-  }
-  for (let n = 0; n < array.length; n++) {
-    for (let i = 0; i < array.length - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        const buff = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = buff;
-      }
-    }
-  }
-
-  return array;
-}
-
-function sortDesc(array) {
-  if (!Array.isArray(array)) {
-    return null;
-  }
-  for (let n = 0; n < array.length; n++) {
-    for (let i = 0; i < array.length - 1; i++) {
-      if (array[i] < array[i + 1]) {
-        const buff = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = buff;
-      }
-    }
-  }
-
-  return array;
+function getSubArray(array, n) {
+  if (array == null) return false;
+  if (n == null) return array[0];
+  return array.slice(0, n);
 }
