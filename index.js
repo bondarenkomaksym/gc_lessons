318873899db26558bd1a3018f27b39cfddd44f8e
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-18 12:56:19 +0300

    master

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function includes(array, n) {
-  return array.indexOf(n) !== -1;
+function uniqueCount(array) {
+  if (!Array.isArray(array)) {
+    return null;
+  }
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === i) {
+      count += 1;
+    }
+  }
+  return count;
 }
