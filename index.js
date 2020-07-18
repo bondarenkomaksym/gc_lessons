b393dd83b620784eb5441408b204db99632de962
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-18 10:21:51 +0300

    master

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function cheker(arr) {
+const nums = [2, 4, 6, 999];
+
+function checker(arr) {
   if (!Array.isArray(arr)) {
     return null;
   }
   return Math.min(...arr) + Math.max(...arr) > 1000 ? true : false;
 }
+console.log(checker(nums));
