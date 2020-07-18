7ec7b15ca37eb78dc4977f142057d0dbf68bb54e
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-18 11:16:19 +0300

    master

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const nums = [1, 4, 20, 12, 5];
-
 function sortAsc(array) {
   if (!Array.isArray(array)) {
     return null;
@@ -16,4 +14,20 @@ function sortAsc(array) {
 
   return array;
 }
-console.log(sortAsc(nums));
+
+function sortDesc(array) {
+  if (!Array.isArray(array)) {
+    return null;
+  }
+  for (let n = 0; n < array.length; n++) {
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] < array[i + 1]) {
+        const buff = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = buff;
+      }
+    }
+  }
+
+  return array;
+}
