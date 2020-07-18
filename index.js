877ecdafbd9f58f439ea24dd399bd5ba8bb81b44
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-18 11:12:10 +0300

    master

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function removeDuplicates(array) {
-  if (!Array.isArray(array)) {
+function sortAsc(arr) {
+  if (!Array.isArray(arr)) {
     return null;
   }
-  return [...new Set(array)];
+  for (let n = 0; n < arr.length; n++) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const buff = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = buff;
+      }
+    }
+  }
+
+  return arr;
 }
