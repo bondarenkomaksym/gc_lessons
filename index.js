167be46f8fe1a1411b2b8ca98e18ad402139bf59
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-18 13:26:04 +0300

    master

--- index.js
+++ index.js
@@ -2,11 +2,5 @@ function uniqueCount(array) {
   if (!Array.isArray(array)) {
     return null;
   }
-  let count = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === i) {
-      count += 1;
-    }
-  }
-  return count;
+  return new Set(array).size;
 }
