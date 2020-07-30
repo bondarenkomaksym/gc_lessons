3838d746ce4342cd9e6a0db2acdb6fce778c75ea
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-30 13:37:07 +0300

    master

--- js7/task10/index.js
+++ js7/task10/index.js
@@ -1,24 +1,19 @@
 "use strict";
 
 // const nums = [2, 5, 6, 3, 0, 3, -1];
-
 // let sum = nums.reduce(function (a, b) {
 //   if (!Array.isArray(nums)) {
 //     return null;
 //   }
 //   return a + b;
 // }, 0);
-
 // console.log(sum);
 
 // const nums = [2, 5, 6, 3, 0, 3, -1];
-
 // let sum = nums.reduce((a, b) => a + b, 0);
-
 // console.log(sum);
 
 const nums = [2, 5, 6, 3, 0, 3, -1];
-
 function sum(nums) {
   if (!Array.isArray(nums)) {
     return null;
