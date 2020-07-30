bfa6fae7ba9cd2aeb058aa3ea7f1c7a79f956a3a
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-30 16:06:30 +0300

    master

--- js7/task1/index.js
+++ js7/task1/index.js
@@ -1,17 +1,34 @@
 "use strict";
 
+// const nums = [2, 5, 6, 3, 0, 3, -1];
+
+// const getSpecialNumbers = (nums) => {
+//   if (!Array.isArray(nums)) {
+//     return null;
+//   }
+//   let specialNumbers = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] % 3 === 0) {
+//       specialNumbers.push(nums[i]);
+//     }
+//   }
+//   return specialNumbers;
+// };
+// console.log(getSpecialNumbers(nums));
+
 const nums = [2, 5, 6, 3, 0, 3, -1];
 
 const getSpecialNumbers = (nums) => {
   if (!Array.isArray(nums)) {
     return null;
   }
+
   let specialNumbers = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] % 3 === 0) {
-      specialNumbers.push(nums[i]);
+  nums.forEach((e) => {
+    if (e % 3 === 0) {
+      specialNumbers.push(e);
     }
-  }
+  });
   return specialNumbers;
 };
 console.log(getSpecialNumbers(nums));
