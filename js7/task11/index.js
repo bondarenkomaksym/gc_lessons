8731ec1a0f3e86b2d5fe4161337b07eef2aa63e7
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-30 13:11:32 +0300

    master

--- js7/task11/index.js
+++ js7/task11/index.js
@@ -1,14 +1,27 @@
 "use strict";
 
+// const people = ["John", "Olivya", "Oleksandr", "Emanuel", "Vanya", "Nastya"];
+
+// function filterNames(names) {
+//   if (!Array.isArray(people)) {
+//     return null;
+//   }
+//   return people.filter(function (el) {
+//     return el.toLowerCase().indexOf(names.toLowerCase()) >= 4;
+//   });
+// }
+
+// console.log(filterNames("ya"));
+
 const people = ["John", "Olivya", "Oleksandr", "Emanuel", "Vanya", "Nastya"];
 
-function filterNames(names) {
+const filterNames = (name) => {
   if (!Array.isArray(people)) {
     return null;
   }
-  return people.filter(function (el) {
-    return el.toLowerCase().indexOf(names.toLowerCase()) >= 4;
+  return people.filter((data) => {
+    return data.toLowerCase().indexOf(name.toLowerCase()) >= 4;
   });
-}
+};
 
 console.log(filterNames("ya"));
