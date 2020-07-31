668e0e5a6dc3dfb87d734982c215b52adfe8e5b6
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-31 13:22:58 +0300

    master

--- js7/task7/index.js
+++ js7/task7/index.js
@@ -6,19 +6,6 @@ function increaseEvenEl(numbers, delta) {
   if (!Array.isArray(numbers)) {
     return null;
   }
-  return numbers.map((x) => (x % 2 !== 0 ? x + delta : x));
+  return numbers.map((x) => (x % 2 === 0 ? x + delta : x));
 }
 console.log(increaseEvenEl(numbers, 20));
-
-// const numbers = [2, 5, 6, 3, 18, 4];
-
-// const increaseEvenEl = (numbers, delta) =>
-//   numbers.map((x) => {
-//     if (x % 2 !== 0) {
-//       return x + delta;
-//     } else {
-//       return x;
-//     }
-//   });
-
-// console.log(increaseEvenEl(numbers, 20));
