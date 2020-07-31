ba09ae5257bc4d331ff310aa1b0c71b43696cd5d
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-31 11:32:39 +0300

    master

---
+++ jspractice/primes/index.js
@@ -0,0 +1,26 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+"use strict";
+
+function isPrime(num) {
+  let isPrime = true;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function getPrimes(n) {
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      console.log(i);
+    }
+  }
+}
+getPrimes(20);
