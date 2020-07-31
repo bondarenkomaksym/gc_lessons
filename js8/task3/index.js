2c000fbf234382f17aa5526912148c15dd921a89
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-31 18:21:51 +0300

    master

---
+++ js8/task3/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const transaction = {
+  currency: "USD",
+  value: 170,
+  operation: "sale",
+  agent: {
+    country: "Ukraine",
+    company: "NYSE",
+  },
+  "operation time": 1584029990001,
+};
+
+console.log(transaction.currency);
+console.log(transaction["value"]);
+const key = "operation";
+console.log([key]);
+console.log(transaction.agent["company"]);
+console.log(transaction.agent.country);
+console.log(transaction["operation time"]);
