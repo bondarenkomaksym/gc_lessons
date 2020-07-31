e1ea3cf449772c51b089b85f62587e8d3d5e158d
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-07-31 14:35:19 +0300

    master

--- js7/task9/index.js
+++ js7/task9/index.js
@@ -1,5 +1,9 @@
 "use strict";
 
-const withdraw = (clients, balances, client, amount) => clients.indexOf(client);
+const withdraw = (clients, balances, client, amount) => {
+  const clientId = clients.indexOf(client);
+  const clientBalance = balances[clientId];
+  return clientBalance > amount ? clientBalance - amount : -1;
+};
 
 console.log(withdraw(["Ann", "John", "User"], [1400, 87, -6], "John", 50));
