0323a305bddbd2518f9a59e97a5d18e4901d980b
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-03 17:38:34 +0300

    master

--- js8/task6/index.js
+++ js8/task6/index.js
@@ -7,36 +7,36 @@
 //input: Obj, key, value
 //output: key prop
 
-// "use strict";
+"use strict";
 
-// const obj = {
-//   name: "Tom",
-// };
-// let newKey = "age";
-// let newValue = 17;
+const obj = {
+  name: "Tom",
+};
+let newKey = "age";
+let newValue = 17;
 
-const addPropertyV1 = (obj, value) => {
+const addPropertyV1 = (obj, key, value) => {
   obj[key] = value;
   return obj;
 };
-// console.log(addPropertyV1(obj, newValue));
+console.log(addPropertyV1(obj, newKey, newValue));
 
-const addPropertyV2 = (obj, value) => {
+const addPropertyV2 = (obj, key, value) => {
   Object.assign(obj, { [key]: value });
   return obj;
 };
-// console.log(addPropertyV2(obj, newValue));
+console.log(addPropertyV2(obj, newKey, newValue));
 
-const addPropertyV3 = (obj, value) => {
+const addPropertyV3 = (obj, key, value) => {
   let clone = Object.assign({}, obj);
   Object.assign(clone, { [key]: value });
   return clone;
 };
-// console.log(addPropertyV3(obj, newValue));
+console.log(addPropertyV3(obj, newKey, newValue));
 
-const addPropertyV4 = (obj, value) => {
+const addPropertyV4 = (obj, key, value) => {
   let clone = { ...obj };
   Object.assign(clone, { [key]: value });
   return clone;
 };
-// console.log(addPropertyV4(obj, newValue));
+console.log(addPropertyV4(obj, newKey, newValue));
