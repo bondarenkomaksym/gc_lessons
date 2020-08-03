95f242befb31dde5d520649f17c7f1148497fac3
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-03 15:55:31 +0300

    master

--- js8/task6/index.js
+++ js8/task6/index.js
@@ -7,16 +7,24 @@
 //input: Obj, key, value
 //output: key prop
 
-// "use strict";
+"use strict";
 
-// const obj = {
-//   name: "Tom",
-// };
-// let newKey = "age";
-// let newValue = 17;
+const obj = {
+  name: "Tom",
+};
+let newKey = "age";
+let newValue = 17;
+
+console.log(obj);
 
 function addPropertyV1(obj, key, value) {
   obj[key] = value;
   return obj;
 }
-// console.log(addPropertyV1(obj, newKey, newValue));
+console.log(addPropertyV1(obj, newKey, newValue));
+
+function addPropertyV2(obj, key, value) {
+  Object.assign(obj);
+  return obj;
+}
+console.log(addPropertyV2(obj, newKey, newValue));
