5441047daab9aa17fa849afa45d9445c56ae815b
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-03 17:37:26 +0300

    master

--- js8/task6/index.js
+++ js8/task6/index.js
@@ -1,21 +1,42 @@
-const addPropertyV1 = (obj, key, value) => {
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input: Obj, key, value
+//output: key prop
+
+// "use strict";
+
+// const obj = {
+//   name: "Tom",
+// };
+// let newKey = "age";
+// let newValue = 17;
+
+const addPropertyV1 = (obj, value) => {
   obj[key] = value;
   return obj;
 };
+// console.log(addPropertyV1(obj, newValue));
 
-const addPropertyV2 = (obj, key, value) => {
+const addPropertyV2 = (obj, value) => {
   Object.assign(obj, { [key]: value });
   return obj;
 };
+// console.log(addPropertyV2(obj, newValue));
 
-const addPropertyV3 = (obj, key, value) => {
+const addPropertyV3 = (obj, value) => {
   let clone = Object.assign({}, obj);
   Object.assign(clone, { [key]: value });
   return clone;
 };
+// console.log(addPropertyV3(obj, newValue));
 
-const addPropertyV4 = (obj, key, value) => {
+const addPropertyV4 = (obj, value) => {
   let clone = { ...obj };
   Object.assign(clone, { [key]: value });
   return clone;
 };
+// console.log(addPropertyV4(obj, newValue));
