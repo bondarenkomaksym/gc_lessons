b15c804361786d4c9e80bbaf2b5a174ae341b0e9
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-03 17:25:17 +0300

    master

--- js8/task6/index.js
+++ js8/task6/index.js
@@ -1,42 +1,21 @@
-// How to work on tech tasks. Step by step guide
-// 1. Learn requirements
-// 2. Create step by step algo (& input/output for funtions)
-// 3. Create prototype solution & testing - черновое решение
-// 4. Final testing & refactoring -> final solution
-
-//input: Obj, key, value
-//output: key prop
-
-"use strict";
-
-const obj = {
-  name: "Tom",
-};
-let newKey = "age";
-let newValue = 17;
-
 const addPropertyV1 = (obj, key, value) => {
   obj[key] = value;
   return obj;
 };
-console.log(addPropertyV1(obj, newKey, newValue));
 
 const addPropertyV2 = (obj, key, value) => {
   Object.assign(obj, { [key]: value });
   return obj;
 };
-console.log(addPropertyV2(obj, newKey, newValue));
 
 const addPropertyV3 = (obj, key, value) => {
   let clone = Object.assign({}, obj);
   Object.assign(clone, { [key]: value });
   return clone;
 };
-console.log(addPropertyV3(obj, newKey, newValue));
 
 const addPropertyV4 = (obj, key, value) => {
   let clone = { ...obj };
   Object.assign(clone, { [key]: value });
   return clone;
 };
-console.log(addPropertyV4(obj, newKey, newValue));
---
+++ js8/task7/index.js
@@ -0,0 +1,29 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input: Objects
+//output: Object
+
+"use strict";
+
+const obj1 = {
+  name: "Tom",
+  age: 17,
+};
+const obj2 = {
+  name: "Bob",
+  student: false,
+};
+
+const mergeObjectsV1 = (obj1, obj2) => {
+  return Object.assign(obj1, obj2);
+};
+console.log(mergeObjectsV1(obj1, obj2));
+
+const mergeObjectsV2 = (obj1, obj2) => {
+  return Object.assign(obj1, obj2);
+};
+console.log(mergeObjectsV2(obj1, obj2));
