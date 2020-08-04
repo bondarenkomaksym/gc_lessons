4f27efca0a450b32d0bb83acf0d465a8be8cbb03
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-04 10:17:19 +0300

    master

--- js8/task7/index.js
+++ js8/task7/index.js
@@ -7,29 +7,29 @@
 //input: Objects
 //output: Object
 
-"use strict";
+// "use strict";
 
-const obj1 = {
-  name: "Tom",
-  age: 17,
-};
-const obj2 = {
-  name: "Bob",
-  student: false,
-};
+// const obj1 = {
+//   name: "Tom",
+//   age: 17,
+// };
+// const obj2 = {
+//   name: "Bob",
+//   student: false,
+// };
 
 const mergeObjectsV1 = (obj1, obj2) => Object.assign({}, obj1, obj2);
-console.log(mergeObjectsV1(obj1, obj2));
+// console.log(mergeObjectsV1(obj1, obj2));
 
 const mergeObjectsV2 = (obj1, obj2) => Object.assign({}, obj2, obj1);
-console.log(mergeObjectsV2(obj1, obj2));
+// console.log(mergeObjectsV2(obj1, obj2));
 
 const mergeObjectsV3 = (obj1, obj2) => {
   return { ...obj1, ...obj2 };
 };
-console.log(mergeObjectsV3(obj1, obj2));
+// console.log(mergeObjectsV3(obj1, obj2));
 
 const mergeObjectsV4 = (obj1, obj2) => {
   return { ...obj2, ...obj1 };
 };
-console.log(mergeObjectsV4(obj1, obj2));
+// console.log(mergeObjectsV4(obj1, obj2));
