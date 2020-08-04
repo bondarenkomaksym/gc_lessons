be7c2f65a6f0d5d11f13d46f2bc8528204ab7dbd
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-04 09:38:36 +0300

    master

--- js8/task7/index.js
+++ js8/task7/index.js
@@ -7,41 +7,41 @@
 //input: Objects
 //output: Object
 
-"use strict";
+// "use strict";
 
-const obj1 = {
-  name: "Tom",
-  age: 17,
-};
-const obj2 = {
-  name: "Bob",
-  student: false,
-};
+// const obj1 = {
+//   name: "Tom",
+//   age: 17,
+// };
+// const obj2 = {
+//   name: "Bob",
+//   student: false,
+// };
 
 const mergeObjectsV1 = (obj1, obj2) => {
   let first = { ...obj1 };
   let second = { ...obj2 };
   return Object.assign(first, second);
 };
-console.log(mergeObjectsV1(obj1, obj2));
+// console.log(mergeObjectsV1(obj1, obj2));
 
 const mergeObjectsV2 = (obj1, obj2) => {
   let first = { ...obj1 };
   let second = { ...obj2 };
   return Object.assign({}, second, first);
 };
-console.log(mergeObjectsV2(obj1, obj2));
+// console.log(mergeObjectsV2(obj1, obj2));
 
 const mergeObjectsV3 = (obj1, obj2) => {
   let first = { ...obj1 };
   let second = { ...obj2 };
   return { ...first, ...second };
 };
-console.log(mergeObjectsV3(obj1, obj2));
+// console.log(mergeObjectsV3(obj1, obj2));
 
 const mergeObjectsV4 = (obj1, obj2) => {
   let first = { ...obj1 };
   let second = { ...obj2 };
   return { ...second, ...first };
 };
-console.log(mergeObjectsV4(obj1, obj2));
+// console.log(mergeObjectsV4(obj1, obj2));
