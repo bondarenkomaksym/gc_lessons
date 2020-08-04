96dbe425694273517536701f13724db003abaa43
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-04 17:14:03 +0300

    master

---
+++ js8/task11/index.js
@@ -0,0 +1,18 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input: massives of strings
+//output: Object
+
+"use strict";
+
+const keysList = ["name", "address", "age"];
+const valuesList = ["Bob", "Ukraine", 34];
+
+const buildObject = (keysList, valuesList) =>
+  keysList.reduce((acc, n, i) => ((acc[n] = valuesList[i]), acc), {});
+
+console.log(buildObject(keysList, valuesList));
