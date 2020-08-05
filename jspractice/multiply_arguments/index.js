1ae9f4d6f1c9bffae89ea9df805811d051769a9d
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-05 11:27:17 +0300

    master

--- jspractice/multiply_arguments/index.js
+++ jspractice/multiply_arguments/index.js
@@ -1,20 +1,48 @@
 // How to work on tech tasks. Step by step guide
-// 1. Learn requirements
-// 2. Create step by step algo (& input/output for funtions)
+// 1. gather all arguments +
+// 2. multiply
 // 3. Create prototype solution & testing - черновое решение
 // 4. Final testing & refactoring -> final solution
 
-//input: Object
-//output: massive
+//input: numbers
+//output: number
 
 "use strict";
 
-function multiply() {
-  let result = 1;
+// function multiply() {
+//   let result = 1;
 
-  for (let i = 0; i < arguments.length; i++) {
-    result *= arguments[i];
-  }
-  return result;
+//   for (let i = 0; i < arguments.length; i++) {
+//     result *= arguments[i];
+//   }
+//   return result;
+// }
+// console.log(multiply(3, 6, 2));
+
+// function multiply(...args) {
+//   // console.log(args);
+//   let res = 1;
+//   args.forEach((e) => {
+//     res *= e;
+//   });
+//   return res;
+// }
+// console.log(multiply(3, 6, 2));
+
+// function multiply(...args) {
+//   // console.log(args);
+//   console.log("reduce start");
+//   const res = args.reduce((res, currentvalue) => {
+//     console.log("reduce callback is called");
+//     console.log(res);
+//     console.log(currentvalue);
+//     return res * currentvalue;
+//   });
+//   return res;
+// }
+// console.log(multiply(3, 6, 2, 4));
+
+function multiply(...args) {
+  return args.reduce((res, currentvalue) => res * currentvalue);
 }
-console.log(multiply(3, 6, 2));
+console.log(multiply(3, 6, 2, 4));
