326291d146553829d9757058ab00122edea5186b
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-06 09:33:45 +0300

    master

---
+++ js10/task3/index.js
@@ -0,0 +1,21 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input: number
+//output: massive
+
+"use strict";
+
+const element = 5.74;
+
+const multiRound = (num) => [
+  Math.floor(num),
+  Math.round(num),
+  Math.ceil(num),
+  Math.trunc(num),
+  +num.toFixed(1),
+];
+console.log(multiRound(element));
