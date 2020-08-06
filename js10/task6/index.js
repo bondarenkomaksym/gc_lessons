787e8f21ff9db46a195bf2257905e654cd12f18b
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-06 13:33:31 +0300

    master

---
+++ js10/task6/index.js
@@ -0,0 +1,31 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:  number, number, number
+//output: massive random numbers
+
+"use strict";
+
+const getRandomNumbers = (length, min, max) => {
+  let result = [];
+
+  for (let i = 0; i < length; i++) {
+    result.push(randomInteger(min, max));
+  }
+  if (result === null) {
+    return null;
+  }
+  return result;
+};
+
+function randomInteger(min, max) {
+  let random = min - 0.5 + Math.random() * (max - min + 1);
+  random = Math.round(random);
+  console.log(random);
+  return random;
+}
+
+console.log(getRandomNumbers(3, 120, 166));
