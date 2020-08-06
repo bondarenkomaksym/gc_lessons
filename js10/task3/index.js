b38dbe817ddb8d9c16d6ca63b2965b66ead97139
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-06 09:39:16 +0300

    master

--- js10/task3/index.js
+++ js10/task3/index.js
@@ -9,13 +9,13 @@
 
 "use strict";
 
-const element = -5.743;
+const element = 5.743;
 
 const multiRound = (num) => [
-  Math.floor(num),
-  Math.round(num),
-  Math.ceil(num),
-  Math.trunc(num),
-  +num.toFixed(1),
+  Math.floor(num * 100) / 100,
+  Math.round(num * 100) / 100,
+  Math.ceil(num * 100) / 100,
+  Math.trunc(num * 100) / 100,
+  +num.toFixed(2),
 ];
 console.log(multiRound(element));
