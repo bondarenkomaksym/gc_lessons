3b51380d08294a1e23677cdf04009f3e3c45ebdc
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-06 19:36:14 +0300

    master

--- js10/task6/index.js
+++ js10/task6/index.js
@@ -21,7 +21,7 @@ const getRandomNumbers = (length, min, max) => {
 };
 
 function randomInteger(min, max) {
-  min = Math.floor(min);
+  min = Math.ceil(min);
   max = Math.floor(max);
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
   // return Math.floor(Math.random() * max) + min;
