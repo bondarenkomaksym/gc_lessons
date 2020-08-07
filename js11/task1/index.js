72c21ec937ef6a8076a26bbcc95c63cb5c83de17
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-07 12:52:31 +0300

    master

--- js11/task1/index.js
+++ js11/task1/index.js
@@ -9,6 +9,24 @@
 
 "use strict";
 
-let text = "Hello World";
+const text = "Hello World";
 
-const splitText = (text, 4);
+const splitText = (text, length) => {
+  let strArr = [];
+  let startPosition = 0;
+  while (true) {
+    let chunk = text.substr(startPosition, length);
+
+    if (chunk.length === 0) {
+      break;
+    }
+
+    strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
+    startPosition += length;
+  }
+
+  return strArr.join("\n");
+};
+
+//1. "absbd csmndc", 4 => ["absb", "d cs"] => ["Absb", "D cs"] => "Absb/nD cs"
+console.log(splitText(text, 4));
