7a386c7c879fae957868c9fa25cd520de51d121e
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-07 08:27:10 +0300

    master

--- js10/task6/index.js
+++ js10/task6/index.js
@@ -9,35 +9,35 @@
 
 "use strict";
 
-const getRandomNumbers = (length, from, to) => {
-  const noNumbersBetween = to - from < 1 && Math.ceil(to) === Math.ceil(from);
-  if (noNumbersBetween) {
-    return null;
-  }
-  return new Array(length)
-    .fill()
-    .map(() => from + (to - from) * Math.random())
-    .map((num) => (num < 0 ? Math.ceil(num) : Math.floor(num)));
-};
-console.log(getRandomNumbers(20, -40.5, -30.8));
-
-// const getRandomNumbers = (length, min, max) => {
-//   let result = [];
-//   if (!Number.isInteger(min, max)) {
+// const getRandomNumbers = (length, from, to) => {
+//   const noNumbersBetween = to - from < 1 && Math.ceil(to) === Math.ceil(from);
+//   if (noNumbersBetween) {
 //     return null;
 //   }
-//   for (let i = 0; i < length; i++) {
-//     result.push(randomInteger(min, max));
-//   }
-//   return result;
+//   return new Array(length)
+//     .fill()
+//     .map(() => from + (to - from) * Math.random())
+//     .map((num) => (num < 0 ? Math.ceil(num) : Math.floor(num)));
 // };
-
-// function randomInteger(min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
-//   // return Math.floor(Math.random() * max) + min;
-//   // return Math.round(Math.random() * (max - min)) + min;
-//   // return Math.round(min + Math.random(max - min));
-// }
 // console.log(getRandomNumbers(20, -40.5, -30.8));
+
+const getRandomNumbers = (length, min, max) => {
+  let result = [];
+  if (!Number.isInteger(min, max)) {
+    return null;
+  }
+  for (let i = 0; i < length; i++) {
+    result.push(randomInteger(min, max));
+  }
+  return result;
+};
+
+function randomInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+  // return Math.floor(Math.random() * max) + min;
+  // return Math.round(Math.random() * (max - min)) + min;
+  return Math.round(min + Math.random() * (max - min));
+}
+console.log(getRandomNumbers(10, 3, 9));
