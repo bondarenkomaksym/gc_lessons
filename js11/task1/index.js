a72a88cf1da51935825b75b92a9673739bfb1c0b
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-07 12:22:56 +0300

    master

--- js11/task1/index.js
+++ js11/task1/index.js
@@ -4,32 +4,11 @@
 // 3. Create prototype solution & testing - черновое решение
 // 4. Final testing & refactoring -> final solution
 
-//input: Object
+//input: string, number
 //output: massive
 
 "use strict";
 
-const elementsList = [2, 5.74, "  6.3", NaN, null, undefined, Infinity];
+let text = "Hello World";
 
-// function getFiniteNumbers(arr) {
-//   let result = arr.map((el) => Number.isFinite(el));
-
-//   return result;
-// }
-
-// console.log(getFiniteNumbers(elementsList));
-
-const getFiniteNumbers = (arr) => arr.filter((el) => Number.isFinite(el));
-console.log(getFiniteNumbers(elementsList));
-
-const getFiniteNumbersV2 = (arr) => arr.filter((el) => isFinite(el));
-console.log(getFiniteNumbersV2(elementsList));
-
-const getNaN = (arr) => arr.filter((el) => Number.isNaN(el));
-console.log(getNaN(elementsList));
-
-const getNaNV2 = (arr) => arr.filter((el) => isNaN(el));
-console.log(getNaNV2(elementsList));
-
-const getIntegers = (arr) => arr.filter((el) => Number.isInteger(el));
-console.log(getIntegers(elementsList));
+const splitText = (text, 4);
