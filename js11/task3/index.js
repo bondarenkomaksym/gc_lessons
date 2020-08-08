42d621db1045957cfb4827622ba696514e2a65b6
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-08 19:49:10 +0300

    master

--- js11/task3/index.js
+++ js11/task3/index.js
@@ -15,35 +15,28 @@
 const str = "lorem ipsum dolor sit amet";
 const length = 4; // длина одной части
 const pattern = new RegExp(".{1," + length + "}", "ig");
-//
 let res = str.match(pattern).map((item) => item.padEnd(length, "."));
 console.log(res);
 
 // var str = "This is a new string!";
-
 // function* chunk(str, len) {
 //   var padded = str.padEnd(Math.ceil(str.length / len) * len, ".");
-
 //   for (var i = 0; i < padded.length / len; i++) {
 //     yield padded.substring(i * len, (i + 1) * len);
 //   }
 // }
-
 // console.log([...chunk(str, 4)]);
 
 // const splitString = (text, length) => {
 //   if (typeof text !== "string") return null;
-
 //   const strArr = [];
 //   let startPosition = 0;
-
 //   if (length === undefined) {
 //     while (true) {
 //       let chunk = text.substr(startPosition, 10);
 //       if (chunk.length === 0) {
 //         break;
 //       }
-
 //       strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
 //       startPosition += 10;
 //     }
@@ -53,13 +46,10 @@ console.log(res);
 //       if (chunk.length === 0) {
 //         break;
 //       }
-
 //       strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
 //       startPosition += length;
 //     }
 //   }
-
 //   return strArr.join(", ");
 // };
-
 // console.log(splitString(text, 4));
