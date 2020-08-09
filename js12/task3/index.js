ca26ccef514d0e5a316c06cbb20e1430e475f9e2
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-09 15:33:18 +0300

    master

--- js12/task3/index.js
+++ js12/task3/index.js
@@ -11,12 +11,22 @@
 
 const arr = ["  1.9 ", "16.4", 17, " 1 dollar "];
 
-const cleanTransactionsList = (arr) => {
-  const pattern = new RegExp(".{1," + 10 + "}", "ig");
-  let res = str.match(pattern).map((item) => item.padEnd(10, "."));
-  return res;
-};
+const cleanTransactionsList = (arr) =>
+  arr.flatMap((element) => {
+    element = String(element).trim();
+    if (/[a-z]/i.test(element)) return [];
+    else return `\$${Number(element).toFixed(2)}`;
+  });
 console.log(cleanTransactionsList(arr));
+
+const cleanTransactionsList2 = (arr) =>
+  arr.filter((e) => !isNaN(e)).map((e) => "$" + (+e).toFixed(2));
+console.log(cleanTransactionsList2(arr));
+
+// Предполагается, что в массиве никогда не будет пустых строк или null (или они преобразуются в "$0.00")
+// filter() берет функцию, вызывает её для каждого элемента массива. Если вызванная функция вернула что-то, что в логическом контексте будет true, оставляет элемент, иначе, удаляет его из будущего массива.
+// isNaN() берет аргумент и пробует преобразовать его в число. Если получается, возвращает false. !false будет true
+// map() - Так же вызывает переданную функцию для каждого элемента массива, и возвращает новый массив, где каждый элемент подменен на результат вызова этой функции. А toFixed(2) сам при необходимости добавит хвост .00
 
 //ищем в строке паттерн указанной длины состоящий из любых символов
 // const pattern = new RegExp(".{1," + length + "}", "ig");
