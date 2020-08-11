37a9807e60a7f2c420c3f2aaf2b1c3568854c9ba
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-11 19:11:25 +0300

    master

--- js15/task1/index.js
+++ js15/task1/index.js
@@ -19,10 +19,8 @@ export default function makeCounter() {
 const counter1 = makeCounter();
 const counter2 = makeCounter();
 
-// console.log(counter1());
-// console.log(counter1());
-// console.log(counter1());
-
-// console.log(counter2());
-// console.log(counter2());
-// console.log(counter2());
+console.log(counter1());
+console.log(counter1());
+console.log(counter2());
+console.log(counter1());
+console.log(counter2());
