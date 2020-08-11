8cb01a3adbedc19b0dd53c10117693b98a8302d4
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-11 14:25:25 +0300

    master

---
+++ js14/task1/index.js
@@ -0,0 +1,26 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input: strings
+//output:
+
+"use strict";
+let message = "Just learn it!";
+
+const sendMessage = (name) => {
+  const sender = "Gromcode";
+  console.log(`${name}, ${message}. Your ${sender}`);
+};
+
+function setMessage(text) {
+  message = text;
+}
+
+sendMessage("Ann");
+
+setMessage("Good job!");
+
+sendMessage("Ann");
