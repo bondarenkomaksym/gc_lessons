835505d8d99e7e265956a890e6a526b94454ed24
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-11 19:05:46 +0300

    master

---
+++ js15/task1/index.js
@@ -0,0 +1,28 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+const makeCounter = () => {
+  let count = 0;
+  return function () {
+    return count++;
+  };
+};
+
+const counter1 = makeCounter();
+const counter2 = makeCounter();
+
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+
+// console.log(counter2());
+// console.log(counter2());
+// console.log(counter2());
