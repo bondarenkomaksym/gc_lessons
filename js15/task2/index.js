92f394d9fff0971a7efbec014511267706bb90dd
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-12 15:11:27 +0300

    master

---
+++ js15/task2/index.js
@@ -0,0 +1,36 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+export function createCalculator() {
+  let memory = 0;
+
+  function add(num) {
+    memory += num;
+  }
+
+  function decrease(num) {
+    memory -= num;
+  }
+
+  function reset() {
+    memory = 0;
+  }
+  function getMemo() {
+    memory;
+  }
+
+  return {
+    add,
+    decrease,
+    reset,
+    getMemo,
+  };
+}
---
+++ jspractice/closure/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+export function makeCounter() {
+  let count = 0;
+  return function () {
+    return count++;
+  };
+}
+
+const counter1 = makeCounter();
+
+// const counter2 = makeCounter();
+
+console.log(counter1());
+console.log(counter1());
+// console.log(counter2());
+// console.log(counter1());
+// console.log(counter2());
+
+//makeCounter lex env
+// {
+//     enviromentRecord: {
+//         count: number,
+//         sender: '...,
+//         sendMessage: func1,
+//     },
+//     outer: globalLexEnv
+// }
