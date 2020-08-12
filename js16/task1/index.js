29fa9db447c47489a05ef2d26e5ebf3d65bb0ba9
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-12 17:36:42 +0300

    master

---
+++ js16/task1/index.js
@@ -0,0 +1,20 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+let a = 17;
+
+if (a) {
+  let a = 1;
+
+  console.log(a);
+}
+
+console.log(a);
