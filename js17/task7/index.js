42d5a4509398717dad1251effd633a660ac9e2d0
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-13 21:29:43 +0300

    master

--- js17/task7/index.js
+++ js17/task7/index.js
@@ -17,10 +17,7 @@ const user = {
   },
   setFullName() {
     let split = this.getFullName().split(" ");
-    return (
-      user.getFullName.bind((this.firstName = split[0])),
-      user.getFullName.bind((this.lastName = split[1]))
-    );
+    return (this.firstName = split[0]), (this.lastName = split[1]);
   },
 };
 // console.log(user.getFullName());
