3f0a1c104f53c13aeb9946555425d86737626134
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-13 14:15:29 +0300

    master

---
+++ js17/task1/index.js
@@ -0,0 +1,36 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+// const user = {
+//   name: "Tom",
+//   age: 17,
+//   sayHi: function () {
+//     console.log(`Hi, I am ${this.name}.`);
+//   },
+// };
+
+// user.walk = function walk() {
+//   console.log(`I'm walking and I am ${this.age}`);
+// };
+
+// user.sayHi();
+// user.walk();
+
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  //если это метод объекта, то "function" можно убрать
+  getFullName() {
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
+};
+
+user.getFullName();
