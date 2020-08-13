2c6c31d9b088ad2f057bf349bd323b592e7d3e1b
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-13 17:54:43 +0300

    master

--- js17/task4/index.js
+++ js17/task4/index.js
@@ -62,26 +62,28 @@ const population = getPopulation.call(country, 43000);
 console.log(population);
 
 /* ===> 4 <=== */
-// const transaction = {
-//   amount: 1200,
-//   operation: "sell",
-//   currency: "USD",
-//   exchange: "NYSE",
-//   printTransaction() {
-//     console.log(
-//       `${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`
-//     );
-//   },
-// };
+const transaction = {
+  amount: 1200,
+  operation: "sell",
+  currency: "USD",
+  exchange: "NYSE",
+  printTransaction() {
+    console.log(
+      `${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`
+    );
+  },
+};
 
-// const anotherTransaction = {
-//   amount: 400,
-//   operation: "buy",
-//   currency: "USD",
-//   exchange: "NASDAQ",
-// };
+const anotherTransaction = {
+  amount: 400,
+  operation: "buy",
+  currency: "USD",
+  exchange: "NASDAQ",
+};
 
 // вызовите метод transaction.printTransaction так, чтобы в консоль вывелось
 // '400 USD - buy on NASDAQ'
 // используйте объект anotherTransaction как контекст
-// ... your code here
+// const result = transaction.printTransaction({ anotherTransaction: amount });
+// result.call();
+transaction.printTransaction.bind(anotherTransaction)();
