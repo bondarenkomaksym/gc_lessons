308b8430aa191bae36404542b7692caa59fd1f55
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-14 18:48:22 +0300

    master

--- js19/task4/index.js
+++ js19/task4/index.js
@@ -15,6 +15,12 @@ const user = {
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
   },
+  set fullName(value) {
+    const [firstName, lastName] = value.split(" ");
+    this.firstName = firstName;
+    this.lastName = lastName;
+  },
 };
+
+user.fullName = "John Doe";
 console.log(user.fullName);
-// export { vehicle, ship };
