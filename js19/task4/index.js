68cdc10c1e798c0b7a6fc08085091f99f2acd11c
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-14 18:22:37 +0300

    master

---
+++ js19/task4/index.js
@@ -0,0 +1,20 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+console.log(user.fullName);
+// export { vehicle, ship };
