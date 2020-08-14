423abed4435a086bff54e6bf01e95d2d7b620045
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-14 14:05:52 +0300

    master

---
+++ js18/task4/index.js
@@ -0,0 +1,23 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input: function
+//output: arr
+
+"use strict";
+
+function saveCalls(innerfunc) {
+  //создаётся массив calls с аргументами с которыми вызвана withMemory
+  withMemory.calls = [];
+  withMemory() {
+    withMemory.call.push([...arguments]);
+
+    return innerfunc.call(this);
+  }
+  // return withMemory;
+}
+
+export { saveCalls };
