fd9f819a9ed3dd88ce6e8fcaf0d2104850288586
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-16 20:24:25 +0300

    master

--- js20/task1/index.js
+++ js20/task1/index.js
@@ -31,39 +31,14 @@ class User {
     }
     return age;
   }
+  createEmpty(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 }
 
 const user = new User("John", 26);
+user.createEmpty("", null);
 console.log(user);
 
-// function User(name, age) {
-//   this.name = name;
-//   this.age = age;
-// }
-//методы нужно хранить в прототипе для экономии памяти
-// User.prototype.sayHi = function () {
-//   console.log(`Hi, I am ${this.name}`);
-// };
-// User.prototype.requestNewPhoto = function () {
-//   console.log(`New photo request was sent for ${this.name}`);
-// };
-// User.prototype.setAge = function (age) {
-//   this.age = age;
-
-//   if (age < 0) {
-//     return false;
-//   }
-
-//   if (age >= 25) {
-//     console.log(`New photo request was sent for ${this.name}`);
-//   }
-//   return age;
-// };
-// const user = new User("John", 26);
-
-// console.log(user);
-// user.sayHi();
-// user.requestNewPhoto();
-// user.setAge(32);
-// console.log(user);
 export { User };
