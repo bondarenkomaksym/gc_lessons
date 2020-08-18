9da785992efc57667174bfc07bbf0025170b66c1
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-18 13:31:06 +0300

    master

---
+++ js21/task1/index.js
@@ -0,0 +1,17 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+const tasks = [
+  { text: "Buy milk", done: false },
+  { text: "Pick up Tom from airport", done: false },
+  { text: "Visit party", done: false },
+  { text: "Visit doctor", done: true },
+  { text: "Buy meat", done: true },
+];
