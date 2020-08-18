1a0a36105d4200d51e98a3fe7a6a750d6acd2d93
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-18 11:32:52 +0300

    master

---
+++ js20/task5/index.js
@@ -0,0 +1,35 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input: strings
+//output:
+
+"use strict";
+
+class User {
+  constructor(id, name, sessionId) {
+    this._id = id;
+    this._name = name;
+    this._sessionId = sessionId;
+  }
+  get id() {
+    return this._id;
+  }
+  get name() {
+    return this._name;
+  }
+  get sessionId() {
+    return this._sessionId;
+  }
+}
+
+class UserRepository {
+  constructor(users) {
+    let users = [];
+  }
+}
+
+// export { User };
---
+++ jspractice/timer/index.js
@@ -0,0 +1,61 @@
+//input: none
+//output: object
+
+// "use strict";
+const timer = {
+  secondsPassed: 0,
+  minsPassed: 0,
+  timerId: 0, //переменная для хранения занчения из setInterval
+  // startTimer() {
+  //   const adder = function () {
+  //     this.secondsPassed += 1;
+  //     console.log(this);
+  //   };
+  //   const adderBind = adder.bind(this); для закрепления контекста
+
+  //   setInterval(adderBind, 1000);
+  // },
+
+  startTimer() {
+    this.timerId = setInterval(() => {
+      console.log(this);
+      this.secondsPassed += 1;
+      // if (this.secondsPassed === 60) {
+      //   this.minsPassed += 1;
+      //   this.secondsPassed = 0;
+      // }
+    }, 1000);
+  },
+
+  getTime() {},
+  stopTimer() {
+    clearInterval(this.timerId);
+  },
+  resetTimer() {
+    this.secondsPassed = 0;
+    this.minsPassed = 0;
+  },
+};
+
+// timer.startTimer();
+
+// setTimeout(() => {
+//   console.log(" + 1 second");
+// }, 1000);
+
+// setInterval(() => {
+//   console.log(" + 1 second");
+// }, 1000);
+
+//loose context example
+// const user = {
+//   name: "Tom",
+//   run() {
+//     console.log(this);
+//   },
+// };
+// user.run();
+
+// const func = user.run;
+// console.log(func); //получаем только функцию run() без имени в объекте
+// func();
