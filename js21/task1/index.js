e7113edbcf1f5cf9d0afbd348e9795ca3be05dd0
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-18 14:45:33 +0300

    master

--- js21/task1/index.js
+++ js21/task1/index.js
@@ -15,3 +15,27 @@ const tasks = [
   { text: "Visit doctor", done: true },
   { text: "Buy meat", done: true },
 ];
+
+const renderListItems = (listItems) => {
+  const listElement = document.querySelector(".list");
+
+  const listItemsElems = listItems
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement("li");
+      listItemElem.classList.add("list__item");
+      if (done) {
+        listItemElem.classList.add("list__item_done");
+      }
+
+      const checkbox = document.createElement("input");
+      checkbox.setAttribute("type", "checkbox");
+      checkbox.checked = done;
+
+      checkbox.classList.add("list__item-checkbox");
+      listItemElem.append(checkbox, text);
+      return listItemElem;
+    });
+  listElement.append(...listItemsElems);
+};
+renderListItems(tasks);
