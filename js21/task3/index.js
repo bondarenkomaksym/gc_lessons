c30e9b25ca26000d189bb0f86f35b79666d3ec34
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-18 16:23:54 +0300

    master

---
+++ js21/task3/index.js
@@ -0,0 +1,25 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+const getItemsList = () => {
+  const elementsList = document.querySelectorAll(".technology");
+  console.dir(elementsList);
+  return elementsList;
+};
+getItemsList();
+
+const getItemsArray = () => {
+  const elementsArray = document.querySelectorAll(".tool");
+  console.dir(elementsArray);
+  return Array.from(elementsArray);
+};
+getItemsArray();
+// export { getTitleElement, getInputElement };
