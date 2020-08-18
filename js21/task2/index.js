6854c9d2153750c75f97a0a0d7b048a4c874bd7d
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-18 15:57:31 +0300

    master

---
+++ js21/task2/index.js
@@ -0,0 +1,23 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+const getTitleElement = () => {
+  const titleElement = document.querySelector(".title");
+  console.dir(titleElement);
+  return titleElement;
+};
+
+const getInputElement = () => {
+  const inputElement = document.querySelector("input", "text");
+  console.dir(inputElement);
+  return inputElement;
+};
+export { getTitleElement, getInputElement };
