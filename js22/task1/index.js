bfb50dc5842ac830e01276ce4467ab5063356296
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-19 18:53:49 +0300

    ьфыеук

--- js22/task1/index.js
+++ js22/task1/index.js
@@ -8,7 +8,7 @@
 //output:
 
 "use strict";
-
+//находим элементы
 const eventsListElem = document.querySelector(".events-list");
 const divElem = document.querySelector(".rect_div");
 const pElem = document.querySelector(".rect_p");
@@ -16,11 +16,11 @@ const spanElem = document.querySelector(".rect_span");
 const clear = document.querySelector(".clear-btn");
 const remove = document.querySelector(".remove-handlers-btn");
 const attach = document.querySelector(".attach-handlers-btn");
-
+//функция для обработки элемента
 const logTarget = (text, color) => {
   eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
 };
-
+//через bind передаём аргументы в logTarget
 const logGreyDiv = logTarget.bind(null, "DIV", "grey");
 const logGreyP = logTarget.bind(null, "P", "grey");
 const logGreySpan = logTarget.bind(null, "SPAN", "grey");
@@ -28,7 +28,7 @@ const logGreySpan = logTarget.bind(null, "SPAN", "grey");
 const logGreenDiv = logTarget.bind(null, "DIV", "green");
 const logGreenP = logTarget.bind(null, "P", "green");
 const logGreenSpan = logTarget.bind(null, "SPAN", "green");
-
+//передаём вышеуказанную функцию в обработчик по нажатию
 divElem.addEventListener("click", logGreyDiv, true);
 divElem.addEventListener("click", logGreenDiv);
 
