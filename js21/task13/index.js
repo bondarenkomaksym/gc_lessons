19b31f668ff7842958acc21ddf7a48137f77e220
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-19 14:56:54 +0300

    master

---
+++ js21/task13/index.js
@@ -0,0 +1,22 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+const getSection = (num) => {
+  const nums = document.querySelector(`span[data-number="${num}"]`);
+  console.log(nums);
+  let parent = nums.closest("div");
+  console.log(parent);
+  let attributes = parent.getAttribute("data-section");
+  console.log(attributes);
+  // return attributes;
+};
+getSection(4);
+// export { getSection };
