59526467a25aa75f47f9e36cddedc9153b4d7e2d
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-19 18:08:04 +0300

    master

---
+++ js22/task1/index.js
@@ -0,0 +1,69 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+const eventsListElem = document.querySelector(".events-list");
+const divElem = document.querySelector(".rect_div");
+const pElem = document.querySelector(".rect_p");
+const spanElem = document.querySelector(".rect_span");
+const clear = document.querySelector(".clear-btn");
+const remove = document.querySelector(".remove-handlers-btn");
+const attach = document.querySelector(".attach-handlers-btn");
+
+const logTarget = (text, color) => {
+  eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
+};
+
+const logGreyDiv = logTarget.bind(null, "DIV", "grey");
+const logGreyP = logTarget.bind(null, "P", "grey");
+const logGreySpan = logTarget.bind(null, "SPAN", "grey");
+
+const logGreenDiv = logTarget.bind(null, "DIV", "green");
+const logGreenP = logTarget.bind(null, "P", "green");
+const logGreenSpan = logTarget.bind(null, "SPAN", "green");
+
+divElem.addEventListener("click", logGreyDiv, true);
+divElem.addEventListener("click", logGreenDiv);
+
+pElem.addEventListener("click", logGreyP, true);
+pElem.addEventListener("click", logGreenP);
+
+spanElem.addEventListener("click", logGreySpan, true);
+spanElem.addEventListener("click", logGreenSpan);
+
+const clearList = () => {
+  eventsListElem.innerHTML = "";
+};
+
+clear.addEventListener("click", clearList);
+
+const attachEventList = () => {
+  divElem.addEventListener("click", logGreyDiv, true);
+  divElem.addEventListener("click", logGreenDiv);
+
+  pElem.addEventListener("click", logGreyP, true);
+  pElem.addEventListener("click", logGreenP);
+
+  spanElem.addEventListener("click", logGreySpan, true);
+  spanElem.addEventListener("click", logGreenSpan);
+};
+attach.addEventListener("click", attachEventList);
+
+const removeEventList = () => {
+  divElem.removeEventListener("click", logGreyDiv, true);
+  divElem.removeEventListener("click", logGreenDiv);
+
+  pElem.removeEventListener("click", logGreyP, true);
+  pElem.removeEventListener("click", logGreenP);
+
+  spanElem.removeEventListener("click", logGreySpan, true);
+  spanElem.removeEventListener("click", logGreenSpan);
+};
+remove.addEventListener("click", removeEventList);
