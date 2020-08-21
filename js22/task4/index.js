8880469822f110b70f06160efb6f888589d823cb
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-21 19:49:10 +0300

    master

--- js22/task4/index.js
+++ js22/task4/index.js
@@ -13,4 +13,5 @@ const check = document.querySelector(".task-status");
 
 check.addEventListener("change", () => {
   check.checked ? console.log(true) : console.log(false);
+  // check.checked ? console.log(check.value) : console.log(check.value);
 });
