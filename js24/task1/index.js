f68a124283875c8ba57b403060a59206ab71e4ed
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-25 12:32:32 +0300

    master

--- js24/task1/index.js
+++ js24/task1/index.js
@@ -8,15 +8,10 @@
 //output: string
 
 "use strict";
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-const dayOfWeek = (date, days) => {
-  const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+export const dayOfWeek = (date, days) => {
   const day = new Date(date).getDate();
-  const dateInFuture = new Date(date).setDate(day + days);
-
-  return weekDays[new Date(dateInFuture).getDay()];
+  const futureDay = new Date(date).setDate(day + days);
+  return weekDays[new Date(futureDay).getDay()];
 };
-dayOfWeek(new Date(2020, 0, 1), 14);
-// console.log(result);
-
-export { dayOfWeek };
