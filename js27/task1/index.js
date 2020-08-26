f75978c5250a2dfcd03a5980e8c18fb353bcaf81
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-26 12:37:23 +0300

    ьфыеук

---
+++ js27/task1/index.js
@@ -0,0 +1,36 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+localStorage.clear();
+
+localStorage.setItem("hobbies", JSON.stringify({ name: "Bruce" }));
+localStorage.setItem("name", JSON.stringify("Bruce"));
+localStorage.setItem("age", JSON.stringify(17));
+
+// console.log(localStorage.getItem("hobbies"));
+
+// console.log(JSON.parse(localStorage.getItem("hobbies")));
+
+const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      //если здесь получим ошибку, то значение парсить не нужно - передаём его в catch
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
+    //значени newValue добавляем в финальный объект
+    return { ...acc, [key]: newValue };
+  }, {});
+};
+
+console.log(getLocalStorageData());
