a0a4707cdee15dee23ebe2984fee310966becc1c
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-27 16:18:20 +0300

    master

---
+++ js28/task1/index.js
@@ -0,0 +1,41 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+const calc = (initialValue) => {
+  let result = initialValue;
+
+  const calculator = {
+    add(value) {
+      result += value;
+      //this это сам объект calculator
+      return this;
+    },
+    mult(value) {
+      result *= value;
+      return this;
+    },
+    substract(value) {
+      result -= value;
+      return this;
+    },
+    div(value) {
+      result /= value;
+      return this;
+    },
+    result() {
+      return result;
+    },
+  };
+  return calculator;
+};
+
+const result = calc(5).add(20).div(43).result();
+// console.log(result);
