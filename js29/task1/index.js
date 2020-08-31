fd8f88028c1095a28cb155b14453d42780b1a7fd
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-08-31 16:53:54 +0300

    master

--- js29/task1/index.js
+++ js29/task1/index.js
@@ -9,19 +9,45 @@
 
 "use strict";
 const addImage = (imgSrc, callback) => {
-  //TODO write your code here
+  //создаём елемент картинки
+  const imgElem = document.createElement("img");
+  //назначаем атрибуты
+  imgElem.setAttribute("alt", "My Photo");
+  //указываем правильный путь для картинки
+  imgElem.src = imgSrc;
+  //методом append добавляем картинку в элемент ".page"
+  const containerElement = document.querySelector(".page");
+  containerElement.append(imgElem);
+  //описываем функцию onImageLoaded 222
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
+  };
+  //подписываемся на событие загрузки картинки 1111
+  imgElem.addEventListener("load", onImageLoaded);
+
+  //сообщение об ошибке
+  imgElem.addEventListener("error", () => callback("Image load is failed..."));
+
+  // console.log({ width, height });
 };
 
+const imgSrc =
+  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
+
 //callack example
-const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = imgElem;
+  const { width, height } = data;
+
   const sizeElem = document.querySelector(".image-size");
   sizeElem.textContent = `${width} x ${height}`;
 };
 
 //test call
-//addImage("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg", onImageLoaded);
+addImage(imgSrc, onImageLoaded);
+//картинка загружается асинхронно!!!!
+export { addImage };
