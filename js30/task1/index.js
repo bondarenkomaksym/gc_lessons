ffa909bd2e2dc467c866a153407254fe4b16488f
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-01 14:46:54 +0300

    master

---
+++ js30/task1/index.js
@@ -0,0 +1,58 @@
+// How to work on tech tasks. Step by step guide
+// 1. Learn requirements
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Create prototype solution & testing - черновое решение
+// 4. Final testing & refactoring -> final solution
+
+//input:
+//output:
+
+"use strict";
+
+export const addImage = (imgSrc) => {
+  const p = new Promise((resolve, reject) => {
+    //создаём елемент картинки
+    const imgElem = document.createElement("img");
+    //указываем правильный путь для картинки
+    imgElem.src = imgSrc;
+    //назначаем атрибуты
+    imgElem.setAttribute("alt", "My Photo");
+    //методом append добавляем картинку в элемент ".page"
+    const containerElement = document.querySelector(".page");
+    containerElement.append(imgElem);
+    //описываем функцию onImageLoaded 222
+    const onImageLoaded = () => {
+      //в случае упеха передаём в промис эту функцию
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+    //подписываемся на событие загрузки картинки 1111
+    imgElem.addEventListener("load", onImageLoaded);
+
+    //сообщение об ошибке, тоже передаём в промис, внутри создаём объект ОШИБКИ
+    imgElem.addEventListener("error", () =>
+      reject(new Error("Image load is failed..."))
+    );
+  });
+  return p;
+};
+
+const imgSrc =
+  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
+
+//callack example
+
+//test call
+const resultPromise = addImage(imgSrc);
+//подписка на результат работы функции с промисом
+resultPromise.then((data) => onImageLoaded(data));
+const onImageLoaded = (data) => {
+  const { width, height } = data;
+  const sizeElem = document.querySelector(".image-size");
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+//
+resultPromise.catch((error) => console.log(error));
+// console.log(result);
+//картинка загружается асинхронно!!!!
