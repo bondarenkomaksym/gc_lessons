9122a54c49964d0fc40dad8148eace64e28fde06
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-18 10:22:09 +0300

    master

--- homepractice/index.js
+++ homepractice/index.js
@@ -114,12 +114,13 @@
 // }
 // console.log(firstNonConsecutive([1, 2, 3, 5]));
 
-function nextId(ids) {
-  for (let i = 0; i < ids.length; i++) {
-    if (ids.indexOf(i) === -1) {
-      return i;
-    }
-  }
-  return ids.length;
-}
-console.log(nextId([0, 1, 2, 3, 5]));
+// function nextId(ids) {
+//   for (let i = 0; i < ids.length; i++) {
+//     if (ids.indexOf(i) === -1) {
+//       return i;
+//     }
+//   }
+//   return ids.length;
+// }
+// console.log(nextId([0, 1, 2, 3, 5]));
+
