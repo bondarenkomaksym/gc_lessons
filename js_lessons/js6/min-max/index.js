3eb13607d37ad52df6650154a1838f8338fbb4db
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-27 18:10:56 +0300

    a

--- js_lessons/js6/min-max/index.js
+++ js_lessons/js6/min-max/index.js
@@ -2,12 +2,14 @@
 
 const nums = [21, 5, 6, 3, 999, 3];
 
-const getSpecialNumbers = (nums) => {
-  if (!Array.isArray(nums)) {
+const checker = (arr) => {
+  if (!Array.isArray(arr)) {
     return null;
   }
-  return (Math.min(...nums) + Math.max(...nums) > 1000) ?
+  return (Math.min(...arr) + Math.max(...arr) > 1000) ?
     true : false;
 
 };
+
+
 console.log(getSpecialNumbers(nums));
